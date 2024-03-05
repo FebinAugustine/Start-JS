70580320578b124120d4901e36a171615343e095
Febin <febinaugustine7@gmail.com>
2024-03-05 14:52:23 +0530

    Comparision finished

---
+++ Basics/04_comparision.js
@@ -0,0 +1,25 @@
+/**
+ SIMPLE COMPARISIONS
+ */
+
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+// Some times gives unpredictable outputs. Avoid these comparisions.
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+// Strict Checks. Compare types also not just values
+console.log("4" === 2);
