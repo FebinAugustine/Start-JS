1065bdb837f103ae8543016bdf36c5b53ece5eb9
Febin <febinaugustine7@gmail.com>
2024-03-05 15:18:37 +0530

    Data type summary an explanation

---
+++ Basics/dataTypesSummary.js
@@ -0,0 +1,46 @@
+// Primitive Type
+
+/*
+7 Types:
+String
+Number
+Boolean
+Null
+Undefined
+Symbol
+BigInt
+*/
+
+// Java Script is a dynamically typed language.
+const score = 100;
+const score2 = 98.5;
+const isLoggedin = false;
+const outsideTemp = null;
+let userEmail = undefined;
+
+const id = Symbol("123");
+const newId = Symbol("123");
+console.log(id == newId);
+
+// Type n at the of the big numbers to auto convert to BigInt.
+const bigNum = 12456789547851455225n;
+
+// Reference Type (Non Primitive)
+
+/**
+Array
+Objects
+Functions 
+*/
+
+const fruits = [apple, orange, mango, banana];
+
+let userObj = {
+  name: "amnu",
+  age: 12,
+  city: "chennai",
+};
+
+const hello = function helloWorld() {
+  console.log("Hello world");
+};
