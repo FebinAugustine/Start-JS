6e45825bc6c82c872fff6adea74ef01fb0ecaa56
Febin <febinaugustine7@gmail.com>
2024-03-12 16:05:09 +0530

    Basics 03: 05 Execution context and call stack finished

---
+++ Basics_03/05_jsExecutionContext.js
@@ -0,0 +1,11 @@
+// Javascript Execution Context. How the flow works.
+
+// First below context get created
+// Global Execution Context
+// Functional Excution Context
+// Evaluation Execution context
+
+// Memmory Alocation Phase: allocate memmory for declared variables
+// Execution Phase: function get executed
+
+// Call stack: First in first out
